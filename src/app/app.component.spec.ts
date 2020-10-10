a93613637a9955fc02b0ffd02ecdce7c8bb8a2b6
paulosantama <paulovitor_pe@hotmail.com>
2020-10-10 17:35:48 -0300

    feat: Criação jogo da velha

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('jogo-da-velha');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jogo-da-velha app is running!');
-  });
 });
